0ca5ec93e63e1b1b895408e297ec795c30cbd856
David L. Day <dday376@gmail.com>
2018-12-29 18:26:56 -0500

    added tests.

---
+++ test/test.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var expect = require("chai").expect;
+var builder = require("../index");
+var fs = require("fs");
+
+describe("#build()", function () {
+
+  it("should return the expected annotated text object", function () {
+    const expected = JSON.parse(fs.readFileSync("./test/annotatedtext.json", "utf8"));
+    const text = fs.readFileSync("./test/test.md", "utf8");
+    const result = builder.build(text);
+    expect(result).to.deep.equal(expected);
+  });
+
+  it("should match the original document exactly", function () {
+    const expected = fs.readFileSync("./test/test.md", "utf8");
+    const annotatedtext = builder.build(expected);
+    const annotation = annotatedtext.annotation;
+    let result = "";
+    for (let node of annotation) {
+      const text = node.text ? node.text : node.markup;
+      result += text;
+    }
+    expect(result).to.equal(expected);
+  });
+
+});
