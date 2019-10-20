0f398e9f1cbd38cd601a277c1193146ffa1b0936
David L. Day <dday376@gmail.com>
2019-10-20 17:39:12 -0400

    added tests for frontmatter

--- test/test.js
+++ test/test.js
@@ -25,4 +25,25 @@ describe("#build()", function () {
     expect(result).to.equal(expected);
   });
 
+  it("should return the expected annotated text object with frontmatter", function () {
+    const expected = JSON.parse(fs.readFileSync("./test/frontmatter-annotatedtext.json", "utf8"));
+    const text = fs.readFileSync("./test/frontmatter.md", "utf8");
+    const result = builder.build(text);
+    // fs.writeFileSync("./test/frontmatter-annotatedtext.json", JSON.stringify(result));
+    expect(result).to.deep.equal(expected);
+  });
+
+  it("should match the original document exactly with frontmatter", function () {
+    const expected = fs.readFileSync("./test/frontmatter.md", "utf8");
+    const annotatedtext = builder.build(expected);
+    const annotation = annotatedtext.annotation;
+    let result = "";
+    for (let node of annotation) {
+      const text = node.text ? node.text : node.markup;
+      result += text;
+    }
+    expect(result).to.equal(expected);
+  });
+
+
 });
