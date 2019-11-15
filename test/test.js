add4be6f3af72cf74416485e9d6bbf6d888aa60e
David L. Day <1132144+davidlday@users.noreply.github.com>
2019-11-14 20:39:34 -0500

    Inline Code (#11)
    * added test cases
    
    * potential solution using intepretation
    
    * use single quotes in place of backticks
    
    * remove unused files
    
    * renamed original test files
    
    * renamed frontmatter test files
    
    * added backtick test case
    
    * missing semicolon

--- test/test.js
+++ test/test.js
@@ -6,15 +6,17 @@ var fs = require("fs");
 
 describe("#build()", function () {
 
+  // Original Test Case
   it("should return the expected annotated text object", function () {
-    const expected = JSON.parse(fs.readFileSync("./test/annotatedtext.json", "utf8"));
-    const text = fs.readFileSync("./test/test.md", "utf8");
+    const expected = JSON.parse(fs.readFileSync("./test/annotatedtext-original.json", "utf8"));
+    const text = fs.readFileSync("./test/test-original.md", "utf8");
     const result = builder.build(text);
+    // fs.writeFileSync("./test/annotatedtext-original-tmp.json", JSON.stringify(result, null, 2));
     expect(result).to.deep.equal(expected);
   });
 
   it("should match the original document exactly", function () {
-    const expected = fs.readFileSync("./test/test.md", "utf8");
+    const expected = fs.readFileSync("./test/test-original.md", "utf8");
     const annotatedtext = builder.build(expected);
     const annotation = annotatedtext.annotation;
     let result = "";
@@ -25,16 +27,17 @@ describe("#build()", function () {
     expect(result).to.equal(expected);
   });
 
+  // Frontmatter Test Case
   it("should return the expected annotated text object with frontmatter", function () {
-    const expected = JSON.parse(fs.readFileSync("./test/frontmatter-annotatedtext.json", "utf8"));
-    const text = fs.readFileSync("./test/frontmatter.md", "utf8");
+    const expected = JSON.parse(fs.readFileSync("./test/annotatedtext-frontmatter.json", "utf8"));
+    const text = fs.readFileSync("./test/test-frontmatter.md", "utf8");
     const result = builder.build(text);
-    // fs.writeFileSync("./test/frontmatter-annotatedtext.json", JSON.stringify(result));
+    // fs.writeFileSync("./test/annotatedtext-frontmatter-tmp.json", JSON.stringify(result, null, 2));
     expect(result).to.deep.equal(expected);
   });
 
   it("should match the original document exactly with frontmatter", function () {
-    const expected = fs.readFileSync("./test/frontmatter.md", "utf8");
+    const expected = fs.readFileSync("./test/test-frontmatter.md", "utf8");
     const annotatedtext = builder.build(expected);
     const annotation = annotatedtext.annotation;
     let result = "";
@@ -45,5 +48,25 @@ describe("#build()", function () {
     expect(result).to.equal(expected);
   });
 
+  // Backtick Test Case
+  it("should return the expected annotated text object with backticks", function () {
+    const expected = JSON.parse(fs.readFileSync("./test/annotatedtext-backticks.json", "utf8"));
+    const text = fs.readFileSync("./test/test-backticks.md", "utf8");
+    const result = builder.build(text);
+    // fs.writeFileSync("./test/annotatedtext-backticks-tmp.json", JSON.stringify(result, null, 2));
+    expect(result).to.deep.equal(expected);
+  });
+
+  it("should match the original document exactly with backticks", function () {
+    const expected = fs.readFileSync("./test/test-backticks.md", "utf8");
+    const annotatedtext = builder.build(expected);
+    const annotation = annotatedtext.annotation;
+    let result = "";
+    for (let node of annotation) {
+      const text = node.text ? node.text : node.markup;
+      result += text;
+    }
+    expect(result).to.equal(expected);
+  });
 
 });
