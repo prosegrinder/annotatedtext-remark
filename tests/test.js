3d72c9f510d7b358afa074659c0f7a026ff0586c
David L. Day <dday376@gmail.com>
2020-10-16 08:38:15 -0400

    moved tests out of src

--- tests/test.js
+++ tests/test.js
@@ -1,22 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 "use strict";
 
 var expect = require("chai").expect;
-var builder = require("../../out/index");
+var builder = require("../out/index");
 var fs = require("fs");
 
 describe("#build()", function () {
-
   // Original Test Case
   it("should return the expected annotated text object", function () {
-    const expected = JSON.parse(fs.readFileSync("./src/test/annotatedtext-original.json", "utf8"));
-    const text = fs.readFileSync("./src/test/test-original.md", "utf8");
+    const expected = JSON.parse(
+      fs.readFileSync("./tests/annotatedtext-original.json", "utf8"),
+    );
+    const text = fs.readFileSync("./tests/test-original.md", "utf8");
     const result = builder.build(text);
-    // fs.writeFileSync("./src/test/annotatedtext-original-tmp.json", JSON.stringify(result, null, 2));
+    // fs.writeFileSync("./tests/annotatedtext-original-tmp.json", JSON.stringify(result, null, 2));
     expect(result).to.deep.equal(expected);
   });
 
   it("should match the original document exactly", function () {
-    const expected = fs.readFileSync("./src/test/test-original.md", "utf8");
+    const expected = fs.readFileSync("./tests/test-original.md", "utf8");
     const annotatedtext = builder.build(expected);
     const annotation = annotatedtext.annotation;
     let result = "";
@@ -29,15 +31,17 @@ describe("#build()", function () {
 
   // Frontmatter Test Case
   it("should return the expected annotated text object with frontmatter", function () {
-    const expected = JSON.parse(fs.readFileSync("./src/test/annotatedtext-frontmatter.json", "utf8"));
-    const text = fs.readFileSync("./src/test/test-frontmatter.md", "utf8");
+    const expected = JSON.parse(
+      fs.readFileSync("./tests/annotatedtext-frontmatter.json", "utf8"),
+    );
+    const text = fs.readFileSync("./tests/test-frontmatter.md", "utf8");
     const result = builder.build(text);
-    // fs.writeFileSync("./src/test/annotatedtext-frontmatter-tmp.json", JSON.stringify(result, null, 2));
+    // fs.writeFileSync("./tests/annotatedtext-frontmatter-tmp.json", JSON.stringify(result, null, 2));
     expect(result).to.deep.equal(expected);
   });
 
   it("should match the original document exactly with frontmatter", function () {
-    const expected = fs.readFileSync("./src/test/test-frontmatter.md", "utf8");
+    const expected = fs.readFileSync("./tests/test-frontmatter.md", "utf8");
     const annotatedtext = builder.build(expected);
     const annotation = annotatedtext.annotation;
     let result = "";
@@ -50,15 +54,17 @@ describe("#build()", function () {
 
   // Escape Character Test Case
   it("should return the expected annotated text object with the escape character", function () {
-    const expected = JSON.parse(fs.readFileSync("./src/test/escape-character.json", "utf8"));
-    const text = fs.readFileSync("./src/test/escape-character.md", "utf8");
+    const expected = JSON.parse(
+      fs.readFileSync("./tests/escape-character.json", "utf8"),
+    );
+    const text = fs.readFileSync("./tests/escape-character.md", "utf8");
     const result = builder.build(text);
-    // fs.writeFileSync("./src/test/escape-character-tmp.json", JSON.stringify(result, null, 2));
+    // fs.writeFileSync("./tests/escape-character-tmp.json", JSON.stringify(result, null, 2));
     expect(result).to.deep.equal(expected);
   });
 
   it("should match the original document exactly with the escape character", function () {
-    const expected = fs.readFileSync("./src/test/escape-character.md", "utf8");
+    const expected = fs.readFileSync("./tests/escape-character.md", "utf8");
     const annotatedtext = builder.build(expected);
     const annotation = annotatedtext.annotation;
     let result = "";
@@ -68,5 +74,4 @@ describe("#build()", function () {
     }
     expect(result).to.equal(expected);
   });
-
 });
